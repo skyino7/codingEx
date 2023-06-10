1b43587f0243252d3d0fb2f94d9be2d7bd36366f
Skyino7 <christambassador12@gmail.com>
2023-06-10 14:34:33 +0000

    Learning about Argc and Agrv

---
+++ argAV.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Number of arguments: %d\n:", argc);
+
+	// print all command-line arguments
+	for (int i = 0; i < argc; i++)
+	{
+		printf("Arguments %d: %s\n", i, argv[i]);
+	}
+
+	return (0);
+}
